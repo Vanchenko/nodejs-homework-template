9543356f793cf205bfb2da085829aab96afc7b48
Vanchenko <vanchenkoev@pakline.com.ua>
2023-08-14 17:47:35 +0300

    small update

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const dataSchema = Joi.object({
-    name: Joi.string().max(30).required(),
-    email: Joi.string().max(40).required(),
-    phone: Joi.string().max(18).required()
+    name: Joi.string().max(30).required().error(new Error("missing required name field")),
+    email: Joi.string().max(40).required().error(new Error("missing required email field")),
+    phone: Joi.string().max(18).required().error(new Error("missing required phone field"))
 })
 
 module.exports = {
